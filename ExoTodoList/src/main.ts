4f03a6f4de174893065358320476ce40af94724e
Warheart-1 <axel97477@gmail.com>
2023-01-10 14:40:45 +0100

    todo list fini

---
+++ ExoTodoList/src/main.ts
@@ -0,0 +1,75 @@
+const list = document.getElementById('list') as HTMLUListElement
+const addToListText = document.getElementById('add-to-list') as HTMLInputElement
+const addBtn = document.getElementById('add-btn') as HTMLButtonElement
+const removeBtn = document.querySelectorAll('.remove') as NodeListOf<HTMLLIElement>
+
+let todos : string[] = []
+
+addBtn.addEventListener('click', () => {
+  addToList(addToListText.value, 'add')
+  addToListText.value = ''
+})
+
+addToListText.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    addToList(addToListText.value, 'add')
+    addToListText.value = ''
+  }
+})
+
+removeBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    removeItem(btn)
+  })
+})
+
+function addToList (text: string, action? : string ) 
+{
+  const li = document.createElement('li')
+  li.innerText = text
+  li.classList.add('remove')
+  li.addEventListener('click', () => {
+    removeItem(li)
+  })
+  list.appendChild(li)
+  if(action === "add"){
+    saveToLocalStoare(text, 'add')
+  }
+}
+
+function removeItem (item: HTMLLIElement)
+{
+  item.remove()
+  saveToLocalStoare(item.innerText, 'remove')
+}
+
+function saveToLocalStoare(text : string, action : string) 
+{
+  if (action === 'remove') {
+    todos = todos.filter((todo) => {
+      return todo !== text
+    })
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }
+  if(action === 'add'){
+    todos.push(text)
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }
+}
+
+function getFromLocalStorage()
+{
+  const reference = localStorage.getItem('todos')
+  if (reference) {
+    todos = JSON.parse(reference)
+    todos.forEach((todo) => {
+      addToList(todo)
+    })
+  }
+}
+
+window.addEventListener('load', () => {
+  getFromLocalStorage()
+})
+
+export {}
